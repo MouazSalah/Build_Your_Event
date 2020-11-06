776cdfe1b6505537c0f8ffdf7516a90856132c6d
Mouaz Salah <mouaz.salah55@yahoo.com>
2020-11-06 19:34:03 +0200

    11/6

--- app/src/main/java/com/buildyourevent/buildyourevent/model/data/aboutus/AboutData.java
+++ app/src/main/java/com/buildyourevent/buildyourevent/model/data/aboutus/AboutData.java
@@ -2,7 +2,7 @@ package com.buildyourevent.buildyourevent.model.data.aboutus;
 
 import com.google.gson.annotations.SerializedName;
 
-public class AboutUsItem {
+public class AboutData {
 
 	@SerializedName("image")
 	private String image;
@@ -16,46 +16,19 @@ public class AboutUsItem {
 	@SerializedName("text")
 	private String text;
 
-	public void setImage(String image){
-		this.image = image;
-	}
-
 	public String getImage(){
 		return image;
-	}
-
-	public void setName(String name){
-		this.name = name;
 	}
 
 	public String getName(){
 		return name;
 	}
 
-	public void setId(int id){
-		this.id = id;
-	}
-
 	public int getId(){
 		return id;
-	}
-
-	public void setText(String text){
-		this.text = text;
 	}
 
 	public String getText(){
 		return text;
 	}
-
-	@Override
- 	public String toString(){
-		return 
-			"DataItem{" + 
-			"image = '" + image + '\'' + 
-			",name = '" + name + '\'' + 
-			",id = '" + id + '\'' + 
-			",text = '" + text + '\'' + 
-			"}";
-		}
 }
